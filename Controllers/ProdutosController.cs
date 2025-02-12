2fe2b59e13396ec11ffe3af4ab1175e43ae3bdb8
Fabrício Carlos Araújo da Silva <fabricio.araujo@corregedoria.df>
2025-02-12 15:18:50 -0300

    segundo commit do dia

--- Controllers/ProdutosController.cs
+++ Controllers/ProdutosController.cs
@@ -22,21 +22,32 @@ namespace APICatalogo.Controllers
             var produtos = _context.Produtos.ToList();
             if (produtos is null)
             {
-                return NotFound("Produtos não encontrados");
+                return NotFound("Produtos não encontrados...");
             }
             return produtos;
         }
 
-        [HttpGet("id:int")]
+        [HttpGet("{id:int}", Name = "ObterProduto")]
         public ActionResult<Produto> Get(int id)
         {
             var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
             if (produto == null)
             {
-                return NotFound();
+                return NotFound("Produto não encontrado...");
             }
             return produto;
+        }
 
+        [HttpPost]
+        public ActionResult Post(Produto produto)
+        {
+            if (produto is null)
+                return BadRequest();
+
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produto);
         }
     }
 }
